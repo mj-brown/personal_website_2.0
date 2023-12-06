c8f3363d34e2c0f6c25bcc0e4f827fa24feb8980
mj-brown <brown8mj5@gmail.com>
2023-12-06 06:12:15 -1000

    finished html and javascript.  Started styling with mobile first

---
+++ script.js
@@ -0,0 +1,49 @@
+// Add click event listeners to each project image
+const projectImages = document.querySelectorAll(".projectsGrid img");
+projectImages.forEach(function (image, index) {
+  image.addEventListener("click", function (event) {
+    // Prevent the default anchor behavior
+    event.preventDefault();
+    // Get title and description based on the clicked image index
+    const title = image.parentElement.previousElementSibling.textContent;
+    const description = getDescriptionByIndex(index);
+
+    // Show the popup
+    showPopup(title, description);
+  });
+});
+
+const getDescriptionByIndex = (index) => {
+  // This function should return the description based on the image index
+  // You can customize this function based on your needs
+  const descriptions = [
+    "Description for Mixed Messages.",
+    "Description for Plumeria Lab Services.",
+    "Description for Kalihi Boogie Board Club.",
+    // Add more descriptions as needed
+  ];
+
+  return descriptions[index] || "No description available.";
+};
+
+const showPopup = (title, description) => {
+  // Create a popup with title, description, and a close button
+  const popup = document.createElement("div");
+  popup.className = "popup";
+  popup.innerHTML = `
+            <h4>${title}</h4>
+            <p>${description}</p>
+            <button onclick="closePopup()">Close</button>
+        `;
+
+  // Append the popup to the body
+  document.body.appendChild(popup);
+};
+
+const closePopup = () => {
+  // Remove the popup from the body
+  const popup = document.querySelector(".popup");
+  if (popup) {
+    document.body.removeChild(popup);
+  }
+};
